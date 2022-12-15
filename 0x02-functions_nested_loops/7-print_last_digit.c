bb8ecf729bb9e24d7d280f3026f957d18e9362f5
DigiXplora <alhassanabdulrahaman@gmail.com>
2022-12-14 21:52:52 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,27 @@
 
 /**
  * prin_last_digit - prints the last digit
- * @n: the number
- * Return: value of the last digit
+ *
+ * @n: the int to print
+ * Return: Always 0
  */
+
 int print_last_digit(int n)
+
 {
-	int ld = n % 10;
+	int last_digit;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 
-	_putchar(ld + '0');
-
-	return (0);
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
