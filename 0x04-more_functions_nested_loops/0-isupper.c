0633db69f9880b3aee30380598e62fe0116bb307
DigiXplora <alhassanabdulrahaman@gmail.com>
2022-12-18 21:21:57 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,15 +6,8 @@
  */
 int _isupper(int c)
 {
-char uppercase = 'A';
-int isupper = 0;
-for (; uppercase <= 'Z'; uppercase++)
-{
-if (c == uppercase)
-{
-isupper = 1;
-break;
-}
-}
-return (isupper);
+	if (c >= 'A' && c <+ 'Z')
+		return (1);
+	else
+		return (0);
 }
